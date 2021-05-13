ba2f718136629f7328265246a428127964992df5
zippyops <zippyopstraining@gmail.com>
2021-05-13 11:48:05 -0400

    semantics commit

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "myec2" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = "t2.micro"
+}
+
+### iam_user.tf
+
+```sh
+resource "aws_instance" "newec2" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = "t2.micro"
+}
